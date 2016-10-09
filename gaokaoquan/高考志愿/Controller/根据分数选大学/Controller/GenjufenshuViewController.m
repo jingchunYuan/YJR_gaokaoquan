858f5527497cd7f8ecbd924bb11038dc6885ebe6
高考圈 <869974213@qq.com>
2016-10-09 16:08:25 +0800

    写了地方都发你gdhg

--- gaokaoquan/高考志愿/Controller/根据分数选大学/Controller/GenjufenshuViewController.m
+++ gaokaoquan/高考志愿/Controller/根据分数选大学/Controller/GenjufenshuViewController.m
@@ -10,13 +10,13 @@
 #import "GenjufenshuViewController.h"
 
 @interface GenjufenshuViewController ()
-
+//北京
 @property (weak, nonatomic) IBOutlet UIButton *cityBtn;
-
+//文科理科
 @property (weak, nonatomic) IBOutlet UIButton *kemuBtn;
-
+//批次
 @property (weak, nonatomic) IBOutlet UIButton *piciBtn;
-
+//选大学
 @property (weak, nonatomic) IBOutlet UITextField *textLb;
 - (IBAction)shiyongBtn:(id)sender;
 
@@ -32,7 +32,33 @@
 -(void)layoutUI{
     
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem initWithImageName:@"返回" highlightedImage:nil title:nil target:self action:@selector(leftBarButtonItemClick)];
+     self.navigationItem.titleView = [self.view titleWithNavigat:@"估分选大学"];
+    
+//    _collegeTxt.borderStyle = UITextBorderStyleLine;
+//    _collegeTxt.layer.masksToBounds = YES;
+//    _collegeTxt.layer.borderColor = [UIColor whiteColor].CGColor;
+//    _collegeTxt.layer.borderWidth = 1;
+    _textLb.borderStyle = UITextBorderStyleLine;
+    _textLb.layer.borderColor = [UIColor whiteColor].CGColor;
+    _textLb.layer.masksToBounds = YES;
+    _textLb.layer.borderColor = [UIColor whiteColor].CGColor;
+    _textLb.layer.borderWidth = 1;
+    _cityBtn.contentEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+    _cityBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    _piciBtn.contentEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+    _piciBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    _kemuBtn.contentEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+    _kemuBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+    
+
+    
 }
+
+- (void)leftBarButtonItemClick
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
